4e8d92997d87c5380378ce556197eaa08fa0400e
dstanko1967 <50676418+dstanko1967@users.noreply.github.com>
2019-10-23 11:36:14 +0200

    Update StudentController.cs

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -11,24 +11,7 @@ namespace Skola.Controllers
 {
     public class StudentController : Controller
     {
-        //static Studenti[] student = new Studenti[]
-        //    {
-        //    new Studenti { Id = 1, Ime = "Petar", Prezime = "Radovanović", JMBG ="0405997740034" , MestoRođenja = "Beograd" },
-        //    new Studenti { Id = 2, Ime = "Miliica", Prezime = "Jovanović", JMBG = "2211997765012", MestoRođenja = "Subotica" },
-        //    new Studenti { Id = 3, Ime = "Predrag", Prezime = "Simonovič", JMBG = "15029977200476", MestoRođenja = "Beograd" }
-        //    };
-        //public IActionResult Index()
-        //{
-
-        //    return View();
-
-        //}
-        //public IActionResult StudentInfo()
-        //{
-
-        //    return View(student);
-
-        //}
+       
         private readonly StudentiContext _context;
 
         public StudentController(StudentiContext context)
